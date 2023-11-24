6c45742f7b69f739ffac4eb7e3c2f9827b139135
Casey Moncur <caseymonc@gmail.com>
2023-11-23 21:21:12 -0700

    Added turn on script

--- scripts/listSerialPorts.js
+++ scripts/listSerialPorts.js
@@ -5,5 +5,23 @@ async function ListPorts () {
   console.log('Ports', ports);
 }
 
-ListPorts();
+async function GetPort(path) {
+  const port = new SerialPort({ path, baudRate: 115200 });
+  await new Promise((resolve, reject) => {
+    port.open((err) => {
+      if (err) return reject(err);
+      resolve();
+    })
+  });
+  return port;
+}
+
+async function TurnOn() {
+  const port = await GetPort('/dev/ttyS0');
+  port.write([0x06, 0x14, 0x00, 0x04, 0x00, 0x34, 0x11, 0x00, 0x00, 0x5D], undefined, (err) => {
+    console.log('Turn On');
+  });
+}
+
+TurnOn();
 
