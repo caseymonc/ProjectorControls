82a853b2b406a700b6cd776573d9a071588e92d0
Casey Moncur <caseymonc@gmail.com>
2023-12-25 22:05:59 -0700

    Fixed a biild issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import 'src/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClientProvider } from 'react-query'
+import queryClient from 'src/lib/queryClient';
+
+import 'src/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient} contextSharing>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
